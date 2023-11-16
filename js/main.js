06eb52b1f8bbd6b4bfb360fefedb3e676e9d6cb2
Moon-YJ <tunjeong@gmail.com>
2023-11-16 13:06:06 +0900

    이벤트, 함수 분리

--- js/main.js
+++ js/main.js
@@ -3,19 +3,12 @@ const mask = document.querySelector('.mask');
 const countEl = mask.querySelector('span');
 const imgNum = 200;
 
-let count = 0;
-
+// 로딩시 (시스템 이벤트)
 const imgs = createImgs(figure, imgNum);
 imgCheck(imgs);
-
-figure.addEventListener('mousemove', (e) => {
-	const { pageX } = e;
-	const percent = parseInt((pageX / window.innerWidth) * imgNum);
-	console.log(percent);
-
-	imgs.forEach((img) => (img.style.visibility = 'hidden'));
-	imgs[percent].style.visibility = 'visible';
-});
+// event객체가 인수로 전달되는 경우에는 wrapping함수로 감싸지 않아도 됨(함수를 정의문 형태로)
+// 마우스무브시 (사용자 이벤트)
+figure.addEventListener('mousemove', showImg);
 
 // 동적 이미지 생성 함수
 function createImgs(figure, imgNum, imgName = 'pic') {
@@ -30,14 +23,26 @@ function createImgs(figure, imgNum, imgName = 'pic') {
 
 // 이미지소스 로딩 완료여부 체크 함수
 function imgCheck(imgs) {
+	let count = 0;
 	imgs.forEach((img) => {
+		// 시스템 이벤트
 		img.addEventListener('error', (e) => {
 			e.currentTarget.setAttribute('src', 'img/logo.png');
 		});
+		// 시스템 이벤트
 		img.addEventListener('load', () => {
 			countEl.innerText = parseInt((count / imgNum) * 100) + 1;
 			count++;
 			if (count === imgNum) mask.remove();
 		});
 	});
+}
+
+// 현재 마우스 포인터 위치에 따른 이미지 노출 처리 함수
+function showImg(e) {
+	const { pageX } = e;
+	const percent = parseInt((pageX / window.innerWidth) * imgNum);
+
+	imgs.forEach((img) => (img.style.visibility = 'hidden'));
+	imgs[percent].style.visibility = 'visible';
 }
